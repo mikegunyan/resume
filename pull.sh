656e35ea45fe02f11a0d33a0ddd4851f94bd77ba
Michael Gunyan <mrgunyan@gmail.com>
2021-10-10 12:31:20 -0600

    Docker Push

---
+++ pull.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "➡ Production Pull started"
+echo "➡ Removing old container"
+docker rm -f mike
+
+echo "➡ Removing old image"
+docker rmi mikegunyan/resume
+
+echo "➡ Pulling new image"
+docker pull mikegunyan/resume
+
+echo "➡ Starting new container"
+docker run -d -p 80:3000 --env HOST=54.241.75.143 --env PASSWORD=crudroot --name mike mikegunyan/resume
+
+echo "➡ Production Pull complete"
