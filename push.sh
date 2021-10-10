95a953199686691b9aeee3018e41f88dbdaf0a38
Michael Gunyan <mrgunyan@gmail.com>
2021-10-10 12:53:33 -0600

    Docker Push

--- push.sh
+++ push.sh
@@ -5,4 +5,4 @@ echo "➡ Production Push started"
 echo "➡ Pushing to Github"
 # branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 git add . && git commit -m"Docker Push"
-git push origin $branch
+git push origin master
